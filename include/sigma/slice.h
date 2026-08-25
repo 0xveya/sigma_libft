a6c92aff0c3dfd8c48f7d5152bed9cb682747ac4
0xveya <ghgstefan@gmail.com>
2026-08-25 02:32:39 +0200

    docs(slice): document public macro

--- include/sigma/slice.h
+++ include/sigma/slice.h
@@ -2,6 +2,7 @@
 
 #include <sigma/qol.h>
 
+/* Defines a borrowed mutable slice type containing a pointer and length. */
 #define SIGMA_SLICE(T, name)                                                   \
   typedef struct name {                                                        \
     usize len;                                                                 \
