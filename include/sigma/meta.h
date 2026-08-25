e71d87b09a07a4f32cae7d04b0192263f7aad937
0xveya <ghgstefan@gmail.com>
2026-08-25 06:34:07 +0200

    feat(meta): add C23 preprocessor metaprogramming primitives

---
+++ include/sigma/meta.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define SIGMA_CAT_I(a, b) a##b
+#define SIGMA_CAT(a, b) SIGMA_CAT_I(a, b)
+
+#define SIGMA_STRINGIFY_I(x) #x
+#define SIGMA_STRINGIFY(x) SIGMA_STRINGIFY_I(x)
+
+#define SIGMA_EMPTY()
+
+#define SIGMA_DEFER(id) id SIGMA_EMPTY()
+
+#define SIGMA_OBSTRUCT(...) __VA_ARGS__ SIGMA_DEFER(SIGMA_EMPTY)()
+
+#define SIGMA_EXPAND_1(...) __VA_ARGS__
+#define SIGMA_EXPAND_2(...)                                                    \
+  SIGMA_EXPAND_1(SIGMA_EXPAND_1(SIGMA_EXPAND_1(__VA_ARGS__)))
+#define SIGMA_EXPAND_4(...)                                                    \
+  SIGMA_EXPAND_2(SIGMA_EXPAND_2(SIGMA_EXPAND_2(__VA_ARGS__)))
+#define SIGMA_EXPAND(...)                                                      \
+  SIGMA_EXPAND_4(SIGMA_EXPAND_4(SIGMA_EXPAND_4(__VA_ARGS__)))
+
+#define SIGMA_FOR_EACH(m, ...)                                                 \
+  __VA_OPT__(SIGMA_EXPAND(SIGMA_FOR_EACH_I(m, __VA_ARGS__)))
+
+#define SIGMA_FOR_EACH_I(m, x, ...)                                            \
+  m(x) __VA_OPT__(SIGMA_OBSTRUCT(SIGMA_FOR_EACH_AGAIN)()(m, __VA_ARGS__))
+
+#define SIGMA_FOR_EACH_AGAIN() SIGMA_FOR_EACH_I
+
+#define SIGMA_REPEAT_1(F, C, N) F(C, N)
+
+#define SIGMA_REPEAT_2(F, C, N)                                                \
+  SIGMA_REPEAT_1(F, C, (N) + 0)                                                \
+  SIGMA_REPEAT_1(F, C, (N) + 1)
+
+#define SIGMA_REPEAT_4(F, C, N)                                                \
+  SIGMA_REPEAT_2(F, C, (N) + 0)                                                \
+  SIGMA_REPEAT_2(F, C, (N) + 2)
+
+#define SIGMA_REPEAT_8(F, C, N)                                                \
+  SIGMA_REPEAT_4(F, C, (N) + 0)                                                \
+  SIGMA_REPEAT_4(F, C, (N) + 4)
